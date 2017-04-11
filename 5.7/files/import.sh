bb6d57f9796b624c0298b1436489f5b18ad7b956
tannerjfco <tannerjfco@gmail.com>
2017-04-11 11:42:11 -0600

    improve multi file import

--- 5.7/files/import.sh
+++ 5.7/files/import.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 dbfile="/db/data.sql"
-files="/db/*.sql"
+files=`find /db -name '*.sql'`
 
 # if sql dump(s) have been mounted into /db combine and import them
-if [ -f $files ]; then
-    cat $files > $dbfile
+if [ -n "$files" ]; then
     echo "Importing database..."
-    MYSQL_PWD=$MYSQL_ROOT_PASSWORD mysql -u root $MYSQL_DATABASE -e "SOURCE $dbfile"
-    rm $dbfile
+    find /db -name '*.sql' | awk '{ print "source",$0 }' | MYSQL_PWD=$MYSQL_ROOT_PASSWORD mysql -u root $MYSQL_DATABASE --batch
+    rm /db/*.sql
 else
     echo "No file to import found."
     exit 1
