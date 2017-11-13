5aea6a0eb86dc778d800bba090a0e53bba3cb0af
Randy Fay <randy@randyfay.com>
2017-11-13 15:30:30 -0700

    Use utf8mb4 as default

--- 5.7/files/entrypoint.sh
+++ 5.7/files/entrypoint.sh
@@ -64,7 +64,7 @@ if [ ! -d "/var/lib/mysql/mysql" ]; then
 	fi
 
 	if [ "$MYSQL_DATABASE" ]; then
-		echo "CREATE DATABASE  IF NOT EXISTS \`$MYSQL_DATABASE\` character set UTF8mb4 collate utf8mb4_bin;" | "${mysql[@]}"
+		echo "CREATE DATABASE IF NOT EXISTS \`$MYSQL_DATABASE\` ;" | "${mysql[@]}"
 		mysql+=( "$MYSQL_DATABASE" )
 	fi
 
